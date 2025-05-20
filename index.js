cbfacfe561424c0ec59e346cf372c5c756f0ff6d
Zak <zak.hajjaji@icloud.com>
2025-05-20 11:22:11 +0100

    Add boiler plate set up for mongoose etc

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+require("dotenv").config(); // loads enviroment variables from a .env file into process.env
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const movieRoutes = require("./routes/movieRoutes");
+const mongoose = require("mongoose");
+
+const MONGODB_URL = process.env.MONGODB_URL;
 
 app.use(express.json()); // middleware to parse JSON requests
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 //GET request
 app.get("/", (request, response) => {
---
+++ schemas/movies.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const moviesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  Genre: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Movie", moviesSchema);
+// mongoose looks at the name, makes it plural, hence why Movie is connected to this schema (will read as movies)
+
+// what properties? producer, genre,
+
+{
+  /* Duration: {
+    type: Number, 
+    required: true,
+  }
+    */
+}
